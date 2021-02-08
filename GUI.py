1dff4b2f7abf5e94e8d29ca509f8f32bd7db204f
David <DavidM.BIZ707@gmail.com>
2021-02-08 15:17:45 -0800

    Add file browsing

--- GUI.py
+++ GUI.py
@@ -1,61 +1,64 @@
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import filedialog as fd
 import os
 from subprocess import Popen, PIPE
 
+def browseFile1():
+   filePath = fd.askopenfilename()
+   location1.insert(0, filePath)
 
-args = ["X:\\Programs\\SolidWorks\\SOLIDWORKS\\SLDWORKS.exe", "/m", "D:\\David\\Desktop\\Senior Design\\Procedural Lens\\Procedural Lens Script.swp"]
-Popen(args, stdout=PIPE, stderr=PIPE)
+args = ["X:\\Programs\\SolidWorks\\SOLIDWORKS\\SLDWORKS.exe", "/m", os.path.dirname(os.path.realpath(__file__)) + "\\Procedural Lens Script.swp"]
+#Popen(args, stdout=PIPE, stderr=PIPE)
 
 root = tk.Tk()
 
 layer1 = tk.Label(root, text="Layer 1")
-layer2 = tk.Label(root, text="Layer 2")
-layer3 = tk.Label(root, text="Layer 3")
+#layer2 = tk.Label(root, text="Layer 2")
+#layer3 = tk.Label(root, text="Layer 3")
 
 layer1.grid(row=0, column=0)
-layer2.grid(row=1, column=0)
-layer3.grid(row=2, column=0)
+#layer2.grid(row=1, column=0)
+#layer3.grid(row=2, column=0)
 
 location1 = tk.Entry(root, width=50)
-location2 = tk.Entry(root, width=50)
-location3 = tk.Entry(root, width=50)
+#location2 = tk.Entry(root, width=50)
+#location3 = tk.Entry(root, width=50)
 
 location1.grid(row=0, column=1, columnspan=6)
-location2.grid(row=1, column=1, columnspan=6)
-location3.grid(row=2, column=1, columnspan=6)
+#location2.grid(row=1, column=1, columnspan=6)
+#location3.grid(row=2, column=1, columnspan=6)
 
-location1.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Layer1.bmp")
-location2.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Layer2.bmp")
-location3.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Layer3.bmp")
+#location1.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Layer1.bmp")
+#location2.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Layer2.bmp")
+#location3.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Layer3.bmp")
 
-button1 = tk.Button(root, text="...", padx=10, pady=5, fg="white", bg="#666666")
-button2 = tk.Button(root, text="...", padx=10, pady=5, fg="white", bg="#666666")
-button3 = tk.Button(root, text="...", padx=10, pady=5, fg="white", bg="#666666")
+button1 = tk.Button(root, text="...", padx=10, pady=5, fg="white", bg="#666666", command=browseFile1)
+#button2 = tk.Button(root, text="...", padx=10, pady=5, fg="white", bg="#666666")
+#button3 = tk.Button(root, text="...", padx=10, pady=5, fg="white", bg="#666666")
 
 button1.grid(row=0, column=7)
-button2.grid(row=1, column=7)
-button3.grid(row=2, column=7)
+#button2.grid(row=1, column=7)
+#button3.grid(row=2, column=7)
 
 label1 = tk.Label(root, text="RES:")
-label2 = tk.Label(root, text="RES:")
-label3 = tk.Label(root, text="RES:")
+#label2 = tk.Label(root, text="RES:")
+#label3 = tk.Label(root, text="RES:")
 
 label1.grid(row=0, column=8)
-label2.grid(row=1, column=8)
-label3.grid(row=2, column=8)
+#label2.grid(row=1, column=8)
+#label3.grid(row=2, column=8)
 
 res1 = tk.Entry(root, width=4)
-res2 = tk.Entry(root, width=4)
-res3 = tk.Entry(root, width=4)
+#res2 = tk.Entry(root, width=4)
+#res3 = tk.Entry(root, width=4)
 
 res1.grid(row=0, column=9)
-res2.grid(row=1, column=9)
-res3.grid(row=2, column=9)
+#res2.grid(row=1, column=9)
+#res3.grid(row=2, column=9)
 
 res1.insert(0, "100")
-res2.insert(0, "50")
-res3.insert(0, "20")
+#res2.insert(0, "50")
+#res3.insert(0, "20")
 
 
 buttonAdd = tk.Button(root, text=" + ", padx=10, pady=5, fg="white", bg="#666666")
@@ -82,7 +85,7 @@ outputBox = tk.Entry(root, width=50)
 outputBox.grid(row=4, column=1, columnspan=6)
 outputBox.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Lens.stl")
 
-generate = tk.Button(root, text="Generate", padx=10, pady=5, fg="white", bg="#666666")
+generate = tk.Button(root, text="Generate", padx=10, pady=5, fg="white", bg="#666666", command=Popen(args, stdout=PIPE, stderr=PIPE))
 generate.grid(row=4, column=7)
 
 
