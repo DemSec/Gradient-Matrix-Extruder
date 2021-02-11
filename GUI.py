b4f5b3b8e06b458ca5adfc103014ef56a7ca6aa9
David <DavidM.BIZ707@gmail.com>
2021-02-10 16:08:13 -0800

    Get SW executable path

--- GUI.py
+++ GUI.py
@@ -1,25 +1,38 @@
-import tkinter as tk
-from tkinter import filedialog as fd
-import os, sys
-from subprocess import Popen, PIPE
-from PIL import Image
-import numpy as np
+import os                             # Locating path
+import winreg                         # Reading file extension assosiations
+from subprocess import Popen, PIPE    # Opening SolidWorks
+import tkinter as tk                  # Creating GUI
+from tkinter import filedialog as fd  # Browsing Files
+from PIL import Image                 # Processing Images
+import numpy as np                    # Reading Images
 
-imagePath1 = ""
-outputPath = os.getcwd() + "\\Output.txt"
+# Get SW executable path:
+file_ext = ".SLDPRT"
+SW_query = winreg.QueryValue(winreg.HKEY_LOCAL_MACHINE, fr"SOFTWARE\Classes\{file_ext}")
+SW_path = winreg.QueryValue(winreg.HKEY_LOCAL_MACHINE, fr"SOFTWARE\Classes\{SW_query}\shell\open\command")
+if SW_path.find("SOLIDWORKS\\") == -1:
+   print("SolidWorks not found, please browse to SolidWorks executable.")
+else:
+   SW_path = SW_path[0:SW_path.find("SOLIDWORKS\\") + 11] + "SLDWORKS.exe"
+
+input_image_path = ""
+output_matrix_path = os.getcwd() + "\\Output.txt"
+output_STL_path = os.getcwd() + "\\Output STL\\"
+#SW_path = "X:\\Programs\\SolidWorks\\SOLIDWORKS\\SLDWORKS.exe"
+macro_path = os.path.dirname(os.path.realpath(__file__)) + "\\Procedural Lens Script.swp"
 
 def browse_image1():
-   global imagePath1
-   imagePath1 = fd.askopenfilename()
-   location1.insert(0, imagePath1)
+   global input_image_path
+   input_image_path = fd.askopenfilename()
+   location1.insert(0, input_image_path)
 
-args = ["X:\\Programs\\SolidWorks\\SOLIDWORKS\\SLDWORKS.exe", "/m", os.path.dirname(os.path.realpath(__file__)) + "\\Procedural Lens Script.swp"]
-#Popen(args, stdout=PIPE, stderr=PIPE)
+def run_macro():
+   Popen([SW_path, "/m", macro_path], stdout=PIPE, stderr=PIPE)
 
 #read an image and return a 2d numpy array of pixels
 def readimage(file):
    im = Image.open(file)
-   imgData = np.array(im.getdata()).reshape(im.size[0],im.size[1],1)
+   img_data = np.array(im.getdata()).reshape(im.size[0],im.size[1],1)
    #print(type(im))
    #print(type(im.size))
    #print(im.size)
@@ -28,13 +41,13 @@ def readimage(file):
    #print(im)
    im.close()
 
-   return imgData
+   return img_data
 
 def generate():
-   global imagePath1
-   data = readimage(imagePath1)
+   global input_image_path
+   data = readimage(input_image_path)
    #print(outputPath)
-   with open(outputPath,"w") as f:
+   with open(output_matrix_path,"w") as f:
       for d in data:
          for p in d:
             num = str(p[0])
@@ -123,12 +136,10 @@ output = tk.Label(root, text="Output")
 output.grid(row=4, column=0)
 outputBox = tk.Entry(root, width=50)
 outputBox.grid(row=4, column=1, columnspan=6)
-outputBox.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Lens.stl")
+#outputBox.insert(0, "C:\\Users\\David\\Desktop\\Lens\\Lens.stl")
 
 generate = tk.Button(root, text="Generate", padx=10, pady=5, fg="white", bg="#666666", command=generate)
 generate.grid(row=4, column=7)
-
-
 
 #openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#666666")
 #openFile.pack()
@@ -137,4 +148,3 @@ generate.grid(row=4, column=7)
 #runApps.pack()
 
 root.mainloop()
-
